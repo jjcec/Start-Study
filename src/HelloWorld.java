2d1206dbd0aaf1f218646ba992d365ddf279e08f
chaeunchae <chaeunchae@192.168.35.141>
2021-01-06 20:28:08 +0900

    문장 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
 		System.out.println("Hello SSAFY");
+		System.out.println("화이팅 합시다!");
 	}
 }
